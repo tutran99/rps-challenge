806062adf3b8b969c6a58ddc5be65f41a1b27d67
Makers Academy <onboarding@makersacademy.com>
2018-10-19 14:24:00 +0000

    Add /play route

--- app.rb
+++ app.rb
@@ -1,15 +1,22 @@
 require 'sinatra/base'
 
 class Rps < Sinatra::Base
+enable :sessions
 get '/' do
   'Testing'
   erb :index
 end
 
 post '/names' do
-  @player = params[:player]
+  session[:player] = params[:player]
+  redirect 'play'
+end
+
+get '/play' do
+  @player = session[:player]
   erb :play
 end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
